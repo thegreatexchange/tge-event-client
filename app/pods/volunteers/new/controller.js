4cc0fac214dd1a9172e7e2ff0d15a8cd57acef86
Brian David Wetzel <briandavidwetzel@gmail.com>
2016-10-18 22:25:24 -0400

    Update registration form

--- app/pods/volunteers/new/controller.js
+++ app/pods/volunteers/new/controller.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  actions: {
+    save() {
+
+    },
+    cancel() {
+      this.transitionToRoute('index');
+    }
+  }
 });
