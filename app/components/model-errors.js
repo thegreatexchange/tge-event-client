dd977269ec8c414856806243b55c1d42cc435f47
Brian David Wetzel <briandavidwetzel@gmail.com>
2016-11-05 15:00:41 -0400

    add model errors component

---
+++ app/components/model-errors.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  model: null,
+
+  errors: Ember.computed('model.errors.[]', function() {
+    let errors = this.get('model.errors').map( function(error) {
+      let attribute = error.attribute.capitalize(),
+          message   = error.message + '.';
+
+      return { attribute: attribute, message: message };
+    });
+    return errors;
+  }),
+
+  hasErrors: Ember.computed('errors.length', function(){
+    return this.get('errors.length') > 0;
+  })
+
+});
